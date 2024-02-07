57ec4fab8b69e5c4a8c9969355b31e9031480a9c
Miguel Leonardo Ortega <migleor@gmail.com>
2024-02-07 12:14:22 -0500

    mejoras contenerizacion

--- init.sql
+++ init.sql
@@ -1,3 +1,9 @@
+-- Crear la base de datos "worldcup"
 CREATE DATABASE worldcup;
+
+-- Conectar a la base de datos "worldcup"
+\c worldcup;
+
+-- Crear el usuario "worldcup" y otorgarle privilegios
 CREATE USER worldcup WITH PASSWORD 'worldcup';
 GRANT ALL PRIVILEGES ON DATABASE worldcup TO worldcup;
